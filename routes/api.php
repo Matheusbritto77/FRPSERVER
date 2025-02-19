d2bb51132f36017c0bc05c92dc5ed64770fbcc74
nortan25 <britto190499@gmail.com>
2025-02-19 17:46:57 -0300

    api config

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/accountinfo', function (Request $request) {
-    return $request->user();
+    return Response::json([
+        "status" => "success",
+        "message" => "Account information retrieved successfully.",
+        "data" => [
+            "user_id" => 12345,
+            "username" => "matheusbritto",
+            "email" => "matheus@exemplo.com",
+            "balance" => 100.50,
+            "currency" => "USD",
+            "account_type" => "reseller",
+            "created_at" => "2024-02-19T12:34:56Z",
+            "updated_at" => "2024-02-19T15:00:00Z"
+        ]
+    ]);
 })->middleware('auth:sanctum');
